02c2cd36a3ecbd3bdb6ae8847bd6c26780fb7207
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-28 12:16:45 +0530

    [ASHIQ] added : Select option by user

--- Program.cs
+++ Program.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Tic_Tac_Toe
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            TicTacToeGame tictactoeGame = new TicTacToeGame();
-            tictactoeGame.Board();
-            Console.ReadLine();
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TicTacToeGame ticTacToeGame = new TicTacToeGame();
+			char[] board = ticTacToeGame.Board();
+			ticTacToeGame.Select();
+			Console.ReadLine();
+
+		}
+	}
 }
--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -2,20 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Tic_Tac_Toe
 {
-    class TicTacToeGame
-    {
-        char[] board = new char[10];
+	class TicTacToeGame
+	{
+		char[] board = new char[10];
 
-        public void Board()
-        {
-            for (int i = 1; i < 10; i++)
-            {
-                board[i] = ' ';
-            }
-        }
-    }
+		public char[] Board()
+		{
+			for (int i = 1; i < 10; i++)
+			{
+				board[i] = ' ';
+			}
+			return board;
+		}
+
+		public void Select()
+		{
+
+
+			Console.WriteLine("Enter your Choice");
+			Console.WriteLine("1. you will choose X");
+			Console.WriteLine("2. you will choose O  ");
+			int option = Convert.ToInt32(Console.ReadLine());
+
+
+			switch (option)
+			{
+				case 1:
+					Console.WriteLine("");
+					char playerOneSelection = 'X';
+					char computer = 'O';
+					break;
+				case 2:
+					Console.WriteLine("");
+					playerOneSelection = 'O';
+					computer = 'X';
+					break;
+				default:
+					break;
+
+			}
+
+		}
+
+	}
 }
